ecf4f9cbc18c517a8b352c43dc8a0bcf825b3643
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-07-31 09:36:14 +0800

    use specific enum types in box example with latest puya version

--- examples/box/contract.py
+++ examples/box/contract.py
@@ -1,10 +1,10 @@
-from algopy import ARC4Contract, Box, OnCompleteAction, TransactionType, UInt64, arc4, op
+from algopy import ARC4Contract, Box, OnCompleteAction, TransactionType, arc4, op
 
 
 class BoxContract(ARC4Contract):
     def __init__(self) -> None:
-        self.oca = Box(UInt64)
-        self.txn = Box(UInt64)
+        self.oca = Box(OnCompleteAction)
+        self.txn = Box(TransactionType)
 
     @arc4.abimethod()
     def store_enums(self) -> None:
@@ -12,8 +12,8 @@ class BoxContract(ARC4Contract):
         self.txn.value = TransactionType.ApplicationCall
 
     @arc4.abimethod()
-    def read_enums(self) -> tuple[UInt64, UInt64]:
+    def read_enums(self) -> arc4.Tuple[arc4.UInt64, arc4.UInt64]:
         assert op.Box.get(b"oca")[0] == op.itob(self.oca.value)
         assert op.Box.get(b"txn")[0] == op.itob(self.txn.value)
 
-        return self.oca.value, self.txn.value
+        return arc4.Tuple((arc4.UInt64(self.oca.value), arc4.UInt64(self.txn.value)))
