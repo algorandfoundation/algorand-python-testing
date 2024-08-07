ed456eaf552c7a0e2d1b298c9277a2becf31c3cc
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-08-07 12:04:11 +0800

    chore: add TODO about subroutine support

--- src/algopy_testing/decorators/subroutine.py
+++ src/algopy_testing/decorators/subroutine.py
@@ -5,5 +5,6 @@ _P = ParamSpec("_P")
 _R = TypeVar("_R")
 
 
+# TODO: set up active app and transaction if subroutine is on a contract
 def subroutine(sub: Callable[_P, _R]) -> Callable[_P, _R]:
     return sub
