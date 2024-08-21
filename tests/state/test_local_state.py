fb808f441c7deee7e92967cd1d61318445e516fc
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2024-08-21 13:46:31 +0800

    test: use correct method for overriding on_completion

--- tests/state/test_local_state.py
+++ tests/state/test_local_state.py
@@ -2,7 +2,6 @@ import algopy
 import algopy_testing
 import pytest
 from algopy_testing._context_helpers.context_storage import algopy_testing_context
-from algopy_testing.enums import OnCompleteAction
 
 from tests.artifacts.StateOps.contract import LocalStateContract
 from tests.common import AVMInvoker
@@ -39,7 +38,7 @@ def test_get_local_arc4_value(
         contract = LocalStateContract()
 
         with ctx.txn.create_group(
-            gtxns=[ctx.any.txn.application_call(on_completion=OnCompleteAction.OptIn)]
+            active_txn_overrides={"on_completion": algopy.OnCompleteAction.OptIn}
         ):
             contract.opt_in()
         test_result = getattr(contract, method_name)(ctx.default_sender)
