2cc15b3dc706eb8585b9658bf67b38da215e2e38
Bobby Lat <bobby.lat@makerx.com.au>
2025-01-20 10:15:13 +0800

    feat: add inline option to subroutine decorator

--- src/_algopy_testing/decorators/subroutine.py
+++ src/_algopy_testing/decorators/subroutine.py
@@ -1,9 +1,25 @@
 from collections.abc import Callable
-from typing import ParamSpec, TypeVar
+from functools import partial, wraps
+from typing import Literal, ParamSpec, TypeVar, overload
 
 _P = ParamSpec("_P")
 _R = TypeVar("_R")
 
 
-def subroutine(sub: Callable[_P, _R]) -> Callable[_P, _R]:
-    return sub
+@overload
+def subroutine(sub: Callable[_P, _R], /) -> Callable[_P, _R]: ...
+@overload
+def subroutine(
+    *, inline: bool | Literal["auto"] = "auto"
+) -> Callable[[Callable[_P, _R]], Callable[_P, _R]]: ...
+def subroutine(
+    sub: Callable[_P, _R] | None = None, *, inline: bool | Literal["auto"] = "auto"
+) -> Callable[_P, _R] | Callable[[Callable[_P, _R]], Callable[_P, _R]]:
+    if sub is None:
+        return partial(subroutine, inline=inline)
+
+    @wraps(sub)
+    def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _R:
+        return sub(*args, **kwargs)
+
+    return wrapper
