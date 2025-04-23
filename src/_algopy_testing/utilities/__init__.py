71c51d1430401ec13d4af61cca5f6bc55d82f9b0
Bobby Lat <bobby.lat@makerx.com.au>
2025-04-23 12:15:10 +0800

    feat: add stub implementation of algopy.size_of function for calculating static storage size of types

--- src/_algopy_testing/utilities/__init__.py
+++ src/_algopy_testing/utilities/__init__.py
@@ -1,4 +1,5 @@
 from _algopy_testing.utilities.budget import OpUpFeeSource, ensure_budget
 from _algopy_testing.utilities.log import log
+from _algopy_testing.utilities.size_of import size_of
 
-__all__ = ["OpUpFeeSource", "ensure_budget", "log"]
+__all__ = ["OpUpFeeSource", "ensure_budget", "log", "size_of"]
